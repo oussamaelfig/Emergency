90a9e85888db42ccd79510ebe0989f120e0208f5
oussama EL FIGHA <oelfigha2002gmail.com>
2022-05-21 21:05:41 -0400

    add some code

--- Urgence.hs
+++ Urgence.hs
@@ -1,17 +1,18 @@
 
 module Main where
-    import System.Environment
+    import System.Environment ( getArgs )
+    import Data.List
 
     -- Constantes :
     tcMinimum = 1
     tcParDefaut = 15
-    
+
     positionArgumentNomFichier = 0
     positionArgumentTc = 1
-    
+
     mssgErreurPremierArgument = "Il faut le nom du fichier comme premier argument."
     mssgErreurDeuxiemeArgument = "Le deuxieme argument doit être plus grand ou égal a " ++ ( show tcMinimum )
-    
+
     -- main
     main = do
                argv <- getArgs
@@ -20,16 +21,16 @@ module Main where
                tc' <- return ( if positionArgumentTc < argc
                                then read (argv !! positionArgumentTc) :: Int
                                else tcParDefaut )
-               tc <- return ( if tcMinimum <= tc' 
-                              then tc' 
+               tc <- return ( if tcMinimum <= tc'
+                              then tc'
                               else error mssgErreurDeuxiemeArgument )
-               contenu <- if positionArgumentNomFichier < argc 
-                          then readFile nomFichierEntrees 
+               contenu <- if positionArgumentNomFichier < argc
+                          then readFile nomFichierEntrees
                           else error mssgErreurPremierArgument
                putStr ( traitement tc contenu )
 
     -- Votre programme ici :
-    
+
     -- La fonction traitement est le point de départ de votre logiciel.
     -- @param tc :: Int
     --        Ce paramètre contient le temps moyen de consultation en minute.
@@ -39,5 +40,34 @@ module Main where
     --        Cette fonction retourne la chaîne de caractères qui sera affichée
     --        a la fin de l'exécution du programme.  Elle devra contenir le nouvel
     --        ordre des patients et la table des fractiles formatés pour l'affichage.
-    traitement :: Int -> String -> String
-    traitement tc contenu = 
+    --traitement :: Int -> String -> String
+    --traitement tc contenu = 
+
+
+
+
+   -- data Patient {patientId :: Int, patienTime :: Int, patientPriority :: Int}
+
+    --if i have a String like :
+    -- "43525 5 2\n25545 7 5\n7455 3 4"
+    -- this will convert it to : 
+    -- ["43525 5 2", "25545 7 5", "7455 3 4"]
+    formaterDonnes :: String -> [String]
+    formaterDonnes = lines
+
+
+
+    -- map words ["43525 5 2", "25545 7 5", "7455 3 4"]
+    -- [["43525","5","2"],["25545","7","5"],["7455","3","4"]]
+    formaterDonnes2 :: [String] -> [[String]]
+    formaterDonnes2 = map words
+
+
+    parserInt :: [[String]] -> [[Int]]
+    parserInt = map (map (read :: String -> Int))
+    
+    sortLGT :: [[Int]] -> [[Int]]
+    sortLGT x y = compare (x!!2) (y!!2) -- compare priorities 
+        <> compare (y!!1) (x!!1) -- compare time in descending order
+        
+    sortBy sortLGT parserInt
