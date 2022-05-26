9f78d97f1524379deaf65f5fd8fe384e7e610987
oussama EL FIGHA <oelfigha2002gmail.com>
2022-05-26 15:37:59 -0400

    add code

--- test.hs
+++ test.hs
@@ -1,4 +1,6 @@
-    import Data.List
+    import Data.List ( elemIndex, sortBy )
+    import Data.Maybe ( catMaybes )
+
 
 
     formaterDonnes :: String -> [String]
@@ -27,3 +29,36 @@
 
     sortString :: String -> [[Int]]
     sortString x = sortBy sortLGT (parserInt (formaterDonnes2 (formaterDonnes x)))
+
+
+    --f(i+1)=t(i+1)+i*15
+
+
+    --display id 
+
+
+
+--     when you have a small number of known shapes, you can extract it with pattern matching, eg
+--   let f list =
+--         case list of
+--           ((_:x:_):_) -> show x
+--           _           -> "bad data"
+--   putStrLn $ f [[43525,5,2],[7455,3,4]] 
+
+
+--[[43525,5,2],[7455,3,4]] !! 0 !! 1
+
+--    getIndicies :: [Int] -> [Int]
+--    getIndicies arr = catMaybes $ map (\elem -> elemIndex elem arr) arr
+
+
+
+--Creating patient data type
+    data Patient = Patient {
+                        patientId :: Int,
+                        patientTemps :: Int,
+                        patientPriorite :: Int
+                        } deriving (Show)  
+
+  --  bar :: [[Int]] -> [[Patient]]
+   -- bar cs = map foo cs
