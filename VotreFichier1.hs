06aaf1a810b384b6a3beb7f14fe625796b725e94
oussa <oelfigha2002@gmail.com>
2022-06-15 13:03:48 -0400

    add

--- VotreFichier1.hs
+++ VotreFichier1.hs
@@ -1,5 +1,5 @@
 -- Auteurs:
--- 
+-- OUSSAMA EL-FIGHA (ELFO74030209)
 -- Zacharie Chenail-Larcher (CHEZ24069706)
 
 module Main where
@@ -99,7 +99,6 @@ module Main where
     --Convertir ma liste des listes de Int en une liste de Tuples contenant les éléments à afficher
     --[[43525,5,2],[7455,3,4],[25545,7,5]] --> [(1,43525,2),(2,7455,4),(3,25545,5)]
     convertTuple :: [[Int]] -> [(Int, Int, Int)]
-    --convertTuple (x:xs) = zipWith (curry (\x -> (fst x,head (snd x), snd x!!2))) [1..] (x:xs)
     convertTuple = zipWith (\i (a:b:c:_) -> (i, a, c)) [1..]
 
 
@@ -121,32 +120,6 @@ module Main where
     premiereRegle :: String -> String
     premiereRegle x = unlines (display' (convertTuple (sortString x)))
 
-
-
-    --Pour tester :
-    -- premiereRegle "43525 5 2\n25545 7 5\n7455 3 4"
-
-
-
-
-    -- **************************************************************************************
-    --Creation d'un data Type Patient
-    data Patient = Patient { patientId :: Int, patientTemps :: Int, patientPriorite :: Int } deriving (Show)
-
-
-    --cette fonction prend la liste des lists de Int et la convertit en une liste des patients
-    -- [[1234, 5, 2], [4568, 7, 3], [7897, 6, 5]] ==>[
-                                                     --Patient {patientId = 1234, patientTemps = 5, patientPriorite = 2},
-                                                     --Patient {patientId = 4568, patientTemps = 7, patientPriorite = 3},
-                                                     --Patient {patientId = 7897, patientTemps = 6, patientPriorite = 5}
-                                                     --]
-    creerListPatient :: [[Int]] -> [Patient]
-    creerListPatient = map (\[a, b, c] -> Patient a b c)
-
-    -- **************************************************************************************
-
-
-
     
     --declaration des constantes: 
     --temps d'attente maximum:
@@ -166,7 +139,6 @@ module Main where
     tupleInfo list a =  zipWith (\[_,x,y] i -> (x+a*i,y)) list [0..]
 
 
-    --groupBy :: ((a0, a2) -> (a1, a2) -> Bool) -> t0 -> [[(Int, Int)]]
 
     --Grouper les elements par prioritée chaque priorité dans une liste à part
     --[(3,2),(17,2),(50,3),(64,3)] ==> [[(3,2),(17,2)],[(50,3),(64,3)]]
@@ -232,26 +204,37 @@ module Main where
 
 
 
-    -- ----------------------------------------------------------------------------------------
+    -- fonction qui prend une liste de fractile et la convertit en liste de tuples pour chaque priorité son fractile
+    -- [0.3, 0.88, 0.68, 0.12] ==> [(2,0.3),(3,0.88),(4,0.68),(5,0.12)]
     convertTupleFrac :: [Double] -> [(Int,Double)]
     convertTupleFrac = zipWith (\i a -> (i, a)) [2..]
 
 
-
+    -- fonction qui prend un tuple de fractile avec leur priorité et la convertit en chaine pour les afficher
+    -- (2,0.3),(3,0.88),(4,0.68),(5,0.12) ==> 2 0.3
+                                            --3 0.88
+                                            --4 0.68
+                                            --5 0.12
     displayFrac :: (Int,Double) -> String
     displayFrac (a,b) = printf "%s" (show a++" "++show b)
 
 
- 
+    --Fonction qui prend la liste des tuple de fractiles avec leurs priorités et la convertit en une chaine 
+    --[(2,0.3),(3,0.88),(4,0.68),(5,0.12)] ==> 2 0.3
+                                            --3 0.88
+                                            --4 0.68
+                                            --5 0.12
     displayFrac' :: [(Int, Double)] -> [String]
     displayFrac' = map displayFrac
 
 
-
+    --Fonction qui combine le tout pour afficher la chaine finale des fractiles avec leurs priorités
+    -- elle prend la chaine initiale avec son temps consultation
     chaineFractile :: String -> Int -> String
     chaineFractile chaine a = unlines (displayFrac'(convertTupleFrac(calculFractil (donneeFractileTout(comparerParPriorite (groupElemMemePriorite (tupleInfo (sortString chaine) a)))))))
-    -------------------------------------------------------------------------------------------------------------------------------
-    -- premiereRegle "43525 5 2\n25545 7 5\n7455 3 4" *> print (algoTriage 20 "43525 5 2\n25545 7 5\n7455 3 4")
+    
+    
+    -- Fontion qui affiche le resultat final les patients ordonnés selon la premiere regle ainsi leurs fractile et la moyenne geometrique
     printResult :: Int -> String -> String
     printResult a chaine = premiereRegle chaine ++ "--------\n" ++ (chaineFractile chaine a) ++ printf "%.4f" (algoTriage a chaine)
 
